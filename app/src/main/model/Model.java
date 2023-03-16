3303884ba6507d9c88074eeb55bb50865bdf4061
Ido Eisenberg <93591649+Eisen125@users.noreply.github.com>
2023-03-16 20:14:56 +0200

    Model show

---
+++ app/src/main/model/Model.java
@@ -0,0 +1,81 @@
+package com.example.Party_Share.model;
+
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Pair;
+
+import androidx.core.os.HandlerCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class Model {
+    private static final Model _instance = new Model();
+
+    private Executor exacutors_single_thread = Executors.newSingleThreadExecutor();
+    private Handler handler = HandlerCompat.createAsync(Looper.getMainLooper());
+    private FirebaseModel firebaseModel = new FirebaseModel();
+    AppLocalDbRepository localDb = AppLocalDb.getAppDb();
+
+    public static Model instance() {
+        return _instance;
+    }
+
+    private Model() {
+
+    }
+
+    public void signOut() {
+        firebaseModel.signOut();
+    }
+
+    public interface Listener<T> {
+        void onComplete(T data);
+    }
+
+    public FirebaseAuth getAuth() {
+        return firebaseModel.autantichation;
+    }
+
+    public void getUserPosts(String label, Listener<List<Post>> callback) {
+        firebaseModel.getUserPosts(label, callback);
+    }
+
+    public void getAllPosts(Listener<List<Post>> callback) {
+        firebaseModel.getAllPosts(callback);
+    }
+
+    public void addPost(Post st, Listener<Void> listener) {
+        firebaseModel.addPost(st, listener);
+    }
+
+    public void uploadImage(String name, Bitmap bitmap, Listener<String> listener) {
+        firebaseModel.uploadImage(name, bitmap, listener);
+    }
+
+    public void signUp(String email, String label, String password, Listener<Pair<Boolean, String>> listener) {
+        firebaseModel.signUp(email, label, password, listener);
+    }
+
+    public void login(String email, String password, Listener<Pair<Boolean, String>> listener) {
+        firebaseModel.login(email, password, listener);
+    }
+
+    public void getPostById(String id, Listener<Post> listener) {
+        firebaseModel.getPostById(id, listener);
+    }
+
+    public FirebaseFirestore getDb() {
+        return firebaseModel.getDatabasefirestore();
+    }
+
+    public void updatePostById(String id, Map<String, Object> updates) {
+        firebaseModel.updatePostByid(id, updates);
+    }
+}
